a9619fdff9e06f966bc78c7262dbdac7803286ec
Vyrapol <vuthavyrapol@gmail.com>
2022-08-13 09:11:36 +0700

    use pg

--- myDB/__init__.py
+++ myDB/__init__.py
@@ -1,33 +1,12 @@
-# import mysql.connector
-from psycopg2 import connect, extras
+from psycopg2 import connect
+from dotenv import dotenv_values
+import os
 
-'''
-For cloud
-
-host="b5grqlbaqnhiu3xs5zqs-mysql.services.clever-cloud.com"
-user="umirqrxz6pdfjjpo"
-passwd="4Lj01OwXEH0e5t7ZKIFU"
-port="3306"
-database="b5grqlbaqnhiu3xs5zqs"
-
-For local
-host="localhost"
-user="root"
-passwd=""
-database="bus_reservation"
-'''
-
-# mydb = mysql.connector.connect(
-#     host="b5grqlbaqnhiu3xs5zqs-mysql.services.clever-cloud.com",
-#     user="umirqrxz6pdfjjpo",
-#     passwd="4Lj01OwXEH0e5t7ZKIFU",
-#     database="b5grqlbaqnhiu3xs5zqs"
-# )
-
-database = "rwmprcfu"
-user = "rwmprcfu"
-password = "PMATMochoBaVnxc9UvDabt-dk8KDcEIQ"
-host = "arjuna.db.elephantsql.com"
+config = dotenv_values(".env")
+database = config['DATABASE'] if config else os.environ.get('DATABASE')
+user = config['USER'] if config else os.environ.get('USER')
+password = config['PASSWORD'] if config else os.environ.get('PASSWORD')
+host = config['HOST'] if config else os.environ.get('HOST')
 
 mydb = connect(database=database, user=user, password=password, host=host)
 mycursor = mydb.cursor()
